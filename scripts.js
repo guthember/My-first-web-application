6dc45778db8924bba7f21277bcedc0fbfa54bc05
guthember@gmail.com <guthember@gmail.com>
2020-04-15 18:38:27 +0200

    Second and Third day together

--- scripts.js
+++ scripts.js
@@ -1,3 +1,74 @@
 $(function(){
-  console.log('elkészült');
-})
+  // photos' datas
+  let imagesDatas = [
+    {
+      photo: 'images/abandoned-forest-industry-nature-34950.jpg',
+      title: 'Forest & industry',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, facere illum cupiditate cumque hic nisi voluptates sint delectus, ullam voluptatum libero officiis quibusdam ipsa maxime! Eius hic dolorum, id optio tempore totam est, incidunt ab excepturi, officia harum sit tenetur!'
+    },
+    {
+      photo: 'images/clouds-daylight-forest-grass-371589.jpg',
+      title: 'Forest with clouds',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, facere illum cupiditate cumque hic nisi voluptates sint delectus, ullam voluptatum libero officiis quibusdam ipsa maxime! Eius hic dolorum, id optio tempore totam est, incidunt ab excepturi, officia harum sit tenetur!'
+    },
+    {
+      photo: 'images/daylight-environment-forest-idyllic-459225.jpg',
+      title: 'Lake in the mirror',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, facere illum cupiditate cumque hic nisi voluptates sint delectus, ullam voluptatum libero officiis quibusdam ipsa maxime! Eius hic dolorum, id optio tempore totam est, incidunt ab excepturi, officia harum sit tenetur!'
+    },
+    {
+      photo: 'images/gray-bridge-and-trees-814499.jpg',
+      title: 'Old bridge',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, facere illum cupiditate cumque hic nisi voluptates sint delectus, ullam voluptatum libero officiis quibusdam ipsa maxime! Eius hic dolorum, id optio tempore totam est, incidunt ab excepturi, officia harum sit tenetur!'
+    },
+    {
+      photo: 'images/nature-forest-trees-fog-4827.jpg',
+      title: 'Foggy forest',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, facere illum cupiditate cumque hic nisi voluptates sint delectus, ullam voluptatum libero officiis quibusdam ipsa maxime! Eius hic dolorum, id optio tempore totam est, incidunt ab excepturi, officia harum sit tenetur!'
+    },
+    {
+      photo: 'images/nature-red-forest-leaves-33109.jpg',
+      title: 'Autumn in Patvarc',
+      description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora, facere illum cupiditate cumque hic nisi voluptates sint delectus, ullam voluptatum libero officiis quibusdam ipsa maxime! Eius hic dolorum, id optio tempore totam est, incidunt ab excepturi, officia harum sit tenetur!'
+    }
+  ];
+
+  // using elements wiht JQuery
+  let $photo = $('#photo-container img');
+  let $title = $('#photo-title');
+  let $description = $('#photo-description');
+  let $left = $('#leftArrow img');
+  let $right = $('#rightArrow img');
+  
+  // own variables
+  let currentImage = 0;
+  
+  function setImage( item ) {
+    $photo.attr('src', imagesDatas[item].photo);
+    $title.text(imagesDatas[item].title);
+    $description.text(imagesDatas[item].description);
+  }
+
+  setImage(currentImage);
+
+  $left.click( function(){
+    if( currentImage > 0) {
+      currentImage--;
+      setImage(currentImage);
+    }
+  });
+ 
+  $right.click( function(){
+    if (currentImage < imagesDatas.length - 1) {
+      currentImage++;
+      setImage(currentImage);
+    }
+  });
+ 
+
+  // $photo.attr('src', imagesDatas[1].photo);
+  // $title.text(imagesDatas[1].title);
+  // $description.text(imagesDatas[1].description);
+
+
+});
